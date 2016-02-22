340513ee2830d3cc23bfafd05e7f1746825d824f
Scott Rogowski <scottmrogowski@gmail.com>
2016-02-21 17:45:39 -0800

    email

--- pearcube/pearcube.py
+++ pearcube/pearcube.py
@@ -3,6 +3,10 @@
 import sys
 
 from flask import Flask, request, render_template
+import sendgrid
+
+SENDGRID_USERNAME = 'app47702827@heroku.com'
+SENDGRID_PASSWORD = '7f2b7fc48527'
 
 app = Flask(__name__)
 
@@ -14,10 +18,18 @@ def index_page():
 def email():
     return render_template('email.html')
 
-# @app.route('/cs_request', method='POST')
-# def cs_request():
-#     request.form['text']
-#     request.form['email']
+@app.route('/cs_request', methods=['POST'])
+def cs_request():
+    sg = sendgrid.SendGridClient(SENDGRID_USERNAME, SENDGRID_PASSWORD)
+
+    message = sendgrid.Mail()
+    message.add_to('scottmrogowski@gmail.com')
+    message.set_subject('test')
+    message.set_text('Body')
+    message.set_from('scott@pearcube.com')
+    status, msg = sg.send(message)
+
+    return ','.join(["POST", request.form.get('body',''), request.form.get('email_address','')])
 
 if __name__ == '__main__':
     kwargs = {}
