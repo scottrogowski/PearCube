b48a986e8d65ec3eeb088f612a1ba610464ac749
Scott Rogowski <scottmrogowski@gmail.com>
2016-03-03 10:06:51 -0800

    mongo fix

--- pearcube/lib/orm.py
+++ pearcube/lib/orm.py
@@ -1,8 +1,15 @@
 import pymongo
 
+import options
+from secrets import MLAB_USERNAME, MLAB_PASSWORD
+
 # Connection to Mongo DB
 try:
-    client = pymongo.MongoClient()
+    if options.DEBUG
+        client = pymongo.MongoClient()
+    else:
+        uri = "mongodb://%s:%s@ds019658.mlab.com:19658/heroku_2wzxbwzm" % (MLAB_USERNAME, MLAB_PASSWORD)
+        client = pymongo.MongoClient(uri)
     print "Connected successfully to mongo"
 except pymongo.errors.ConnectionFailure, e:
    print "Could not connect to MongoDB: %s" % e 
