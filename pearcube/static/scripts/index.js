5cd0140d8693a4d5c3151a7172adb9b595c362fd
Scott Rogowski <scottmrogowski@gmail.com>
2016-03-02 17:57:13 -0800

    form errors look nice

--- pearcube/static/scripts/index.js
+++ pearcube/static/scripts/index.js
@@ -26,37 +26,21 @@ var EMAIL_REGEX = new RegExp('\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b');
 function send_cs_email() {
     var $cs_form_ta = $('#cs-form-ta')
     var $cs_form_in = $('#cs-form-in')
-    var are_problems = false
+    var problems_exist = false
 
     if ($cs_form_ta.val().length < 3) {
         $cs_form_ta.addClass('form-problems')
-        $cs_form_ta.animate({'backgroundColor': '#555555'}, 1000, 'linear',
-            function() {
-                console.log('done!')
-            });
-        $cs_form_ta.animate({'backgroundColor': '#ffffff'}, 1000,             'linear',
-            function() {
-                console.log('done!')
-            });
-        are_problems = true;
-        console.log("too short")
-    }
-    else {
-        $cs_form_ta.removeClass('form-problems')
+        $cs_form_ta.focus(function () {$cs_form_ta.removeClass('form-problems')})
+        problems_exist = true;
     }
 
     if ($cs_form_in.val().length < 3 || !(EMAIL_REGEX.test($cs_form_in.val()))) {
         $cs_form_in.addClass('form-problems')
-        $cs_form_in.css({'backgroundColor': 'red'})
-        $cs_form_in.animate({'backgroundColor': '#ffffff'}, 1000)
-        $cs_form_in.animate({'background-color': '#ffffff'}, 1000)
-        are_problems = true;
-    }
-    else {
-        $cs_form_in.removeClass('form-problems')
+        $cs_form_in.focus(function () {$cs_form_in.removeClass('form-problems')})
+        problems_exist = true;
     }
 
-    if (are_problems) return false;
+    if (problems_exist) return false;
 
     $.ajax({
         url: '/cs_request',
