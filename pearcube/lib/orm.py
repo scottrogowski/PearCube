6993495a3c6d20f9088986eb248318c3ddf5a9bc
Scott Rogowski <scottmrogowski@gmail.com>
2016-03-03 10:20:31 -0800

    on debug or prod?

--- pearcube/lib/orm.py
+++ pearcube/lib/orm.py
@@ -7,12 +7,13 @@ from secrets import MLAB_USERNAME, MLAB_PASSWORD
 try:
     if options.DEBUG:
         client = pymongo.MongoClient()
+        print "Connected successfully to mongo debug"
     else:
         uri = "mongodb://%s:%s@ds019658.mlab.com:19658/heroku_2wzxbwzm" % (MLAB_USERNAME, MLAB_PASSWORD)
         client = pymongo.MongoClient(uri)
-    print "Connected successfully to mongo"
+        print "Connected successfully to mongo prod"
 except pymongo.errors.ConnectionFailure, e:
-   print "Could not connect to MongoDB: %s" % e 
+   print "Could not connect to mongo: %s" % e 
    exit(1)
 
 db = client.db
